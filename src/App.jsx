4af6f021d956d1b2676c0eb0195c6eadcabd9098
Troy Chryssos <troychryssos@gmail.com>
2020-09-09 18:59:48 -0400

    setup preact and parcel structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { h } from 'preact'
+
+const App = () => {
+	return (
+		<div>heres my app</div>
+	)
+}
+
+export default App
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { h, render } from 'preact'
+import App from '/src/App.jsx'
+
+const mount = document.querySelector('#app')
+render(<App />, mount)
