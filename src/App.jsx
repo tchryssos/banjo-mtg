f0d7222691b010eb05bdf137e5a4a00584dec91a
Troy Chryssos <troychryssos@gmail.com>
2020-09-21 10:48:40 -0400

    babel auto added to app, but removing the rc file seems to work well

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import 'regenerator-runtime/runtime'
+import 'regenerator-runtime/runtime'
 import { h } from 'preact'
 import { useContext } from 'preact/hooks'
 import * as classes from './App.css'
